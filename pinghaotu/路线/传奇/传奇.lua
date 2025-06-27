e6f3b2e55591ca03c88b00f1dd00c571e100ccdb
Kunkun <2456368409@qq.com>
2025-06-27 21:36:02 +0800

    1

--- pinghaotu/路线/传奇/传奇.lua
+++ pinghaotu/路线/传奇/传奇.lua
@@ -1,7 +1,7 @@
-monster: [6,3], pos: [19054, 16599]
-monster: [6,3], pos: [6889, 41559]
-monster: [6,3], pos: [18132, 64223],narrow:1
-monster: [6,3], pos: [17082, 51928]
+monster: [6,1], pos: [19054, 16599]
+monster: [6,1], pos: [6889, 41559]
+monster: [6,1], pos: [18132, 64223],narrow:1
+monster: [6,1], pos: [17082, 51928]
 
 
 
