97db78f49179f83ad7d1b378247490df64490e94
Kunkun <2456368409@qq.com>
2025-12-18 21:56:37 +0800

    1

--- pinhaotu/路线/y7/y7.lua
+++ pinhaotu/路线/y7/y7.lua
@@ -1,46 +1,46 @@
-monster: [9, 1], pos: [2382, 46067], narrow: 5--yxg
-monster: [9, 1], pos: [23561, 20247], narrow: 5--yxg
-monster: [9, 1], pos: [46353, 21845], narrow: 5--yxg
-monster: [9, 1], pos: [45739, 33772], narrow: 5--yxg
-monster: [9, 1], pos: [27223, 46149], narrow: 5--yxg
-monster: [9, 1], pos: [29835, 32419] --yxg
-monster: [8, 3], pos: [35985, 16057]--cy
-monster: [8, 3], pos: [35213, 31231] ,narrow: 5--cy
-monster: [8, 3], pos: [42512, 24550] ,narrow: 5--cy
-monster: [8, 3], pos: [37134, 46641] ,narrow: 5--cy
-monster: [8, 3], pos: [47506, 40534] ,narrow: 5--cy
-monster: [8, 3], pos: [47070, 52133] ,narrow: 5--cy
-monster: [5, 2], pos: [39260, 26353]--fd
-monster: [5, 2], pos: [45611, 57912] ,narrow: 1--fd
-monster: [4, 3], pos: [35644, 48901]--fd
-monster: [4, 3], pos: [39977, 58117]--fd
-monster: [4, 3], pos: [35264, 59428] ,narrow: 1--fd
-monster: [4, 1], pos: [21819, 26476], wait: 120--fd
-monster: [4, 1], pos: [31013, 28730], select: 2--fd
-monster: [5, 3], pos: [44791, 16394]--fd
-monster: [4, 2], pos: [22204, 27952]--ly
-monster: [4, 2], pos: [32896, 39370]--ly
-monster: [4, 2], pos: [48402, 47788]--ly
-monster: [11, 2], pos: [31269, 45370]--ly
-monster: [11, 2], pos: [44484, 45452]--ly
-monster: [8, 2], pos: [43306, 57748] --ly
-monster: [6, 3], pos: [46072, 20902]--xm
-monster: [6, 3], pos: [40540, 12582]--xm
-monster: [6, 3], pos: [36647, 21312], wait: 120--xm
-monster: [7, 1], pos: [41539, 14714]--xm
-monster: [6, 3], pos: [2279, 44264]--xm
-monster: [6, 2], pos: [38875, 40821]--xm
-monster: [6, 2], pos: [54497, 25042] --xm
-monster: [6, 2], pos: [46609, 20574], narrow: 1--xm
-monster: [10, 2], pos: [27812, 60371], narrow: 1--dq
-monster: [10, 3], pos: [34138, 52748]--dq
-monster: [9, 3], pos: [4456, 0]--dq
-monster: [9, 2], pos: [31474, 26640]--dq
-monster: [11, 1], pos: [29374, 22829]--dq
-monster: [10, 1], pos: [27710, 33034]--dq
-monster: [2, 3], pos: [14674, 6640]--nt
-monster: [3, 3], pos: [23023, 23525]--nt
-monster: [3, 1], pos: [39516, 17091]--nt
-monster: [3, 1], pos: [43331, 4795], narrow: 1, wait:120--nt
-monster: [2, 1], pos: [7043, 37337]--nt
+area:yxg, pos: [4200, 46272], narrow: 5--yxg
+area:yxg, pos: [25072, 20492], narrow: 5 --yxg
+area:yxg, pos: [48095, 21968] --yxg
+area:yxg, pos: [47275, 34386] --yxg
+area:yxg, pos: [29195, 46846] --yxg
+area:yxg, pos: [32601, 32460], narrow: -1 --yxg
+area:dx , pos: [32755, 32583], narrow: 5--cy
+area:dx , pos: [32729, 41436] --cy
+area:dx , pos: [40719, 34960] --cy
+area:dx , pos: [34778, 56723] --cy
+area:dx , pos: [45944, 50944] --cy
+area:dx , pos: [44740, 62338] --cy
+area:fd ,resnar:1, pos: [45560, 5041] ,narrow: 2 --fd
+monster: syxclfszz, pos: [45611, 57912] ,narrow: 1--fd
+monster: sxhr, pos: [35644, 48901]--fd
+monster: sxhr, pos: [39977, 58117]--fd
+monster: sxhr, pos: [35264, 59428] ,narrow: 1--fd
+monster: mxdj, pos: [21819, 26476], wait: 120--fd
+monster: mxdj, pos: [31013, 28730], select: 2--fd
+monster: tjrhhd, pos: [44791, 16394]--fd
+monster: ysns, pos: [22204, 27952]--ly
+monster: ysns, pos: [32896, 39370]--ly
+monster: ysns, pos: [48402, 47788]--ly
+monster: wxzb, pos: [31269, 45370]--ly
+monster: wxzb, pos: [44484, 45452]--ly
+monster: cds, pos: [43306, 57748] --ly
+monster: fssc, pos: [46072, 20902]--xm
+monster: fssc, pos: [40540, 12582]--xm
+monster: fssc, pos: [36647, 21312], wait: 120--xm
+monster: wxzc, pos: [41539, 14714]--xm
+monster: fssc, pos: [2279, 44264]--xm
+monster: szjlz, pos: [38875, 40821]--xm
+monster: szjlz, pos: [54497, 25042] --xm
+monster: szjlz, pos: [46609, 20574], narrow: 1, wait:120 --xm
+monster: hcjgzl, pos: [27812, 60371], narrow: 1--dq
+monster: wxzh, pos: [34138, 52748]--dq
+monster: lyqx, pos: [4456, 0]--dq
+monster: hjws, pos: [31474, 26640]--dq
+monster: mojg, pos: [29374, 22829]--dq
+monster: wxzs, pos: [27710, 33034]--dq
+monster: ssmjz, pos: [14674, 6640]--nt
+monster: tsnylsw, pos: [23023, 23525]--nt
+monster: gxx, pos: [39516, 17091]--nt
+monster: gxx, pos: [43331, 4795], narrow: 1, wait:120--nt
+monster: ryhlx, pos: [7043, 37337]--nt
 area:ss,pos: [45585, 30616],resnar: 2 ,areawait: true --nt
